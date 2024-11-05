d8d7c870629bca552711bc9d4f269c8ec59dc64a
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2024-11-05 14:28:28 +0100

    Fixed ex

--- ApacheLucene/MRI_2024_25/src/main/java/di/uniba/it/mri2324/lucene/HelloWorldEx.java
+++ ApacheLucene/MRI_2024_25/src/main/java/di/uniba/it/mri2324/lucene/HelloWorldEx.java
@@ -28,91 +28,92 @@ import java.util.logging.Logger;
 // Try some queries, and check if the number of documents matching the query is correct
 
 /**
-*
-* @author marco
-*/
+ * @author marco
+ */
 public class HelloWorldEx {
 
-   /**
-    * @param args the command line arguments
- * @throws ParseException 
-    */
-	public static void main(String[] args) throws ParseException {
-       try {
-           //Open a directory from the file system (index directory)
-           FSDirectory fsdir = FSDirectory.open(new File("./resources/documenti_news").toPath());
+    /**
+     * @param args the command line arguments
+     * @throws ParseException
+     */
+    public static void main(String[] args) throws ParseException {
+        try {
+            //Open a directory from the file system (index directory)
+            FSDirectory fsdir = FSDirectory.open(new File("./resources/documenti_news").toPath());
 
-           //IndexWriter configuration
-           IndexWriterConfig iwc = new IndexWriterConfig(new StandardAnalyzer());
-           
-           //Index directory is created if not exists or overwritten
-           iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-           
-           
-           //Create IndexWriter
-           IndexWriter writer = new IndexWriter(fsdir, iwc);
-           //Create document and add fields
-           
-           
-           Document doc = new Document();
-           doc.add(new TextField("titolo", "Articolo Web Numero 1", Field.Store.YES));
-           doc.add(new TextField("introduzione", "questa è l'introduzione del mio documento", Field.Store.YES));
-           doc.add(new TextField("contenuto", "questo è il contenuto del mio documento", Field.Store.NO));
-           doc.add(new TextField("commenti", "questo è un commento di un utente di esempio", Field.Store.NO));
-           writer.addDocument(doc);
+            //IndexWriter configuration
+            IndexWriterConfig iwc = new IndexWriterConfig(new StandardAnalyzer());
 
-           doc.add(new TextField("titolo", "Articolo Web Numero 2", Field.Store.YES));
-           doc.add(new TextField("introduzione", "questa è l'introduzione del tuo documento", Field.Store.YES));
-           doc.add(new TextField("contenuto", "questo è il contenuto del tuo documento", Field.Store.NO));
-           doc.add(new TextField("commenti", "questo è un altro commento di un utente di esempio", Field.Store.NO));
-           writer.addDocument(doc);
-
-           doc.add(new TextField("titolo", "Articolo Web Numero 3", Field.Store.YES));
-           doc.add(new TextField("introduzione", "questa è l'introduzione del suo documento", Field.Store.YES));
-           doc.add(new TextField("contenuto", "questo è il contenuto del suo documento", Field.Store.NO));
-           doc.add(new TextField("commenti", "questo è un altro ancora commento di un utente di esempio", Field.Store.NO));
-           writer.addDocument(doc);
+            //Index directory is created if not exists or overwritten
+            iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
 
 
-           doc.add(new TextField("titolo", "Articolo Web Numero 4", Field.Store.YES));
-           doc.add(new TextField("introduzione", "questa è l'introduzione del nostro documento", Field.Store.YES));
-           doc.add(new TextField("contenuto", "questo è il contenuto del nostro documento", Field.Store.NO));
-           doc.add(new TextField("commenti", "questo è un commento di due utenti di esempio", Field.Store.NO));
-           writer.addDocument(doc);
+            //Create IndexWriter
+            IndexWriter writer = new IndexWriter(fsdir, iwc);
 
 
-           doc.add(new TextField("titolo", "Articolo Web Numero 5", Field.Store.YES));
-           doc.add(new TextField("introduzione", "questa è l'introduzione del vostro documento", Field.Store.YES));
-           doc.add(new TextField("contenuto", "questo è il contenuto del vostro documento", Field.Store.NO));
-           doc.add(new TextField("commenti", "questo è un commento di un utente di esempio, forse", Field.Store.NO));
-           writer.addDocument(doc);
+            //Create document and add fields
+            Document doc = new Document();
+            doc.add(new TextField("titolo", "Articolo Web Numero 1", Field.Store.YES));
+            doc.add(new TextField("introduzione", "questa è l'introduzione del mio documento", Field.Store.YES));
+            doc.add(new TextField("contenuto", "questo è il contenuto del mio documento", Field.Store.NO));
+            doc.add(new TextField("commenti", "questo è un commento di un utente di esempio", Field.Store.NO));
+            writer.addDocument(doc);
 
-           //close IndexWriter
-           writer.close();
+            doc = new Document();
+            doc.add(new TextField("titolo", "Articolo Web Numero 2", Field.Store.YES));
+            doc.add(new TextField("introduzione", "questa è l'introduzione del tuo documento", Field.Store.YES));
+            doc.add(new TextField("contenuto", "questo è il contenuto del tuo documento", Field.Store.NO));
+            doc.add(new TextField("commenti", "questo è un altro commento di un utente di esempio", Field.Store.NO));
+            writer.addDocument(doc);
 
-           // Crea un IndexReader
-           IndexReader reader = DirectoryReader.open(fsdir);
+            doc = new Document();
+            doc.add(new TextField("titolo", "Articolo Web Numero 3", Field.Store.YES));
+            doc.add(new TextField("introduzione", "questa è l'introduzione del suo documento", Field.Store.YES));
+            doc.add(new TextField("contenuto", "questo è il contenuto del suo documento", Field.Store.NO));
+            doc.add(new TextField("commenti", "questo è un altro ancora commento di un utente di esempio", Field.Store.NO));
+            writer.addDocument(doc);
 
-           // Conta il numero di documenti attivi (non cancellati)
-           int numDocs = reader.numDocs();
-           System.out.println("Numero di documenti nell'indice: " + numDocs);
+            doc = new Document();
+            doc.add(new TextField("titolo", "Articolo Web Numero 4", Field.Store.YES));
+            doc.add(new TextField("introduzione", "questa è l'introduzione del nostro documento", Field.Store.YES));
+            doc.add(new TextField("contenuto", "questo è il contenuto del nostro documento", Field.Store.NO));
+            doc.add(new TextField("commenti", "questo è un commento di due utenti di esempio", Field.Store.NO));
+            writer.addDocument(doc);
 
-           //Create the IndexSearcher
-           IndexSearcher searcher = new IndexSearcher(DirectoryReader.open(fsdir));
+            doc = new Document();
+            doc.add(new TextField("titolo", "Articolo Web Numero 5", Field.Store.YES));
+            doc.add(new TextField("introduzione", "questa è l'introduzione del vostro documento", Field.Store.YES));
+            doc.add(new TextField("contenuto", "questo è il contenuto del vostro documento", Field.Store.NO));
+            doc.add(new TextField("commenti", "questo è un commento di un utente di esempio, forse", Field.Store.NO));
+            writer.addDocument(doc);
 
-           //Create the query parser with the default field and analyzer
-           QueryParser qp = new QueryParser("commenti", new StandardAnalyzer());
-           
-           //Parse the query
-           Query q = qp.parse("ancora");
-           
-           //Search
-           TopDocs topdocs = searcher.search(q, 10);
-           System.out.println("Found " + topdocs.totalHits.value + " document(s).");
-           
-       } catch (IOException ex) {
-           Logger.getLogger(HelloWorldEx.class.getName()).log(Level.SEVERE, null, ex);
-       }
-   }
+            //close IndexWriter
+            writer.close();
+
+            // Crea un IndexReader
+            IndexReader reader = DirectoryReader.open(fsdir);
+
+            // Conta il numero di documenti attivi (non cancellati)
+            int numDocs = reader.numDocs();
+            System.out.println("Numero di documenti nell'indice: " + numDocs);
+
+            //Create the IndexSearcher
+            IndexSearcher searcher = new IndexSearcher(DirectoryReader.open(fsdir));
+
+            //Create the query parser with the default field and analyzer
+            QueryParser qp = new QueryParser("titolo", new StandardAnalyzer());
+
+            //Parse the query
+            Query q = qp.parse("3");
+
+            //Search
+            TopDocs topdocs = searcher.search(q, 10);
+            System.out.println("Found " + topdocs.totalHits.value + " document(s).");
+
+        } catch (IOException ex) {
+            Logger.getLogger(HelloWorldEx.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
 }
